d658b73f28d356cfc89a54f21beaa600698a875e
jongsoo1203 <114615490+jongsoo1203@users.noreply.github.com>
2024-02-18 17:32:43 -0500

    getting the response data

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Calendar from "./Calendar";
+import EventFinder from "./api-requests";
+// Fetch events from the UMBInvolved API
+// Get the queries
+
+
 
 export default function Home() {
   return (
