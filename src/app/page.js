b4adb87c2ebf55125494e9a57e54e2a99cb4e7c1
max_moskalenko <mmoskalenko21@gmail.com>
2024-02-18 19:05:29 -0500

    final build

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,26 @@
-
+"use client"
 import Image from "next/image";
-import styles from "./page.css";
+import "./page.css";
 import Calendar from "./Calendar";
+import { useState } from "react";
 import EventFinder from "./api-requests";
+
 // Fetch events from the UMBInvolved API
 // Get the queries
 
-
-
-
 export default function Home() {
+  const [events, setEvents] = useState([]); // Initialize the events array state here
 
+  // Function to handle updating the events array
+  const updateEvents = (newEvents) => {
+    setEvents(newEvents);
+  };
 
-    return (
-        <main className="main">
-    
-            <Calendar />
-    </main>
+  return (
+    <div className="main">
+      {/* Pass the events array and the update function as props to Calendar */}
+      <Calendar events={events} updateEvents={updateEvents} />
+      {console.log(events)}
+    </div>
   );
 }
